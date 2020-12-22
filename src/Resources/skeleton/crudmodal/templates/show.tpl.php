da1822dd33c314e5349885887e5e77e388f6ea31
sbyaute <sbollard74@gmail.com>
2020-12-22 14:46:38 +0100

    feature/modal
    add new features
     - Custom template
     - Delete with modal mode
     - Add crudmodal command to create controller and template with modal mode

--- src/Resources/skeleton/crudmodal/templates/show.tpl.php
+++ src/Resources/skeleton/crudmodal/templates/show.tpl.php
@@ -2,9 +2,13 @@
 
 {% block page_content %}
     <!-- start of modal -->
-        {% if modalDelete is defined and modalDelete is not empty %}
+    {% if modal is defined and modal is not empty %}
+        {{ include('<?= $route_name ?>/_modal.html.twig', { modal: modal } ) }}
+    {% endif %}
+    <!-- end of modal -->
+    {% if modalDelete is defined and modalDelete is not empty %}
         {{ include('<?= $route_name ?>/_modal_delete.html.twig', { modal: modalDelete } ) }}
-        {% endif %}
+    {% endif %}
     <!-- end of modal -->
 
     <div class="row">
@@ -26,7 +30,7 @@
     {% endblock %}
                 {% block box_footer %}
                     <div class="pull-right">
-                        <a class="btn btn-primary " href="{{ path('<?= $route_name ?>_edit', {'<?= $entity_identifier ?>': <?= $entity_twig_var_singular ?>.<?= $entity_identifier ?>}) }}"><i class='fas fa-edit'></i> {{ 'Edit'|trans }}</a>
+                        <button class="btn btn-primary "data-toggle="modal" data-target="#modalEditNew"><i class='fas fa-edit'></i> {{ 'Edit'|trans }}</button>
                         <a class="btn btn-warning " href="{{ path('<?= $route_name ?>_index') }}"><i class='fas fa-arrow-alt-circle-left'></i> {{ 'Back to list'|trans }}</a>
                         <button type="button" class="btn btn-danger"  data-toggle="modal" data-target="#modalDelete"><i class='fas fa-trash'></i> {{ 'Delete'|trans }}</button>
                     </div>
